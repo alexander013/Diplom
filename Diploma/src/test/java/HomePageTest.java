5cfd5d332ef0bba5b651bdd3fb07346b7a2977d2
KurochkinAV <kurochkin_013@list.ru>
2023-01-09 17:42:24 +0300

    Остановился на банере УЖЕ В ПРОДАЖЕ

--- Diploma/src/test/java/HomePageTest.java
+++ Diploma/src/test/java/HomePageTest.java
@@ -5,40 +5,33 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
-public class TestHomePage {
+
+public class HomePageTest
+{
     private WebDriver driver;
     private WebDriverWait wait;
 
 
-//    Необходимо сделать так чтобы браузер открывался один раз
+//    Необходимо сделать так, чтобы браузер открывался один раз
     @Before
-    public void setUp() throws InterruptedException {
+    public void setUp()
+    {
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         driver = new ChromeDriver();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized");
-//        ChromeDriver driver = new ChromeDriver(options);
-        //        options.add_argument(r"user-data-dir=C:\Users\Eugene\AppData\Local\Google\Chrome\User Data\Profile 4")
-//        browser = webdriver.Chrome(executable_path=r'C:\Users\Eugene\Documents\Python\chromedriver.exe', chrome_options=options)
-//
-//        browser.get('https://www.google.com/')
-////        Открытие браузера во весь экран
+//        Открытие браузера во весь экран
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 13);
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//        driver.manage().timeouts().pageLoadTimeout(13, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 2);
     }
 
     @After
-    public void tearDown() throws IOException {
+    public void tearDown() throws IOException
+    {
         var sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(sourceFile, new File("c:\\tmp\\screenshot.png"));
         driver.quit();
@@ -53,7 +46,7 @@ public class TestHomePage {
 //----------------------------------------------------------------------------------------------------------------------
 //  ТЕСТ НА ПЕРЕХОД ПО ССЫЛКЕ http://intershop5.skillbox.ru/ И ПОПАДАНИЕ НА ГЛАВНУЮ СТРАНИЦУ САЙТА
     @Test
-    public void TestHomePage_1()
+    public void OpenHomePageTest()
     {
 //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
@@ -68,11 +61,10 @@ public class TestHomePage {
 //----------------------------------------------------------------------------------------------------------------------
 //  ТЕСТ НА ПЕРЕХОД ИЗ РАЗДЕЛА "Главная" В РАЗДЕЛ "Каталог" И ОБРАТНО НА ГЛАВНУЮ СТРАНИЦУ
     @Test
-    public void TestHomePage_2() throws InterruptedException {
+    public void GoingToCatalogTest()
+    {
 //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
-//        Использовал ожидание 1.5 секунды чтобы появились все элементы
-        Thread.sleep(1500);
 //        Элемент для перехода в раздел "Главная"
         var HomeElementLocator = By.cssSelector("li#menu-item-26");
 //        Элемент для перехода в раздел "Каталог"
@@ -102,11 +94,10 @@ public class TestHomePage {
 //----------------------------------------------------------------------------------------------------------------------
 //  ТЕСТ НА ПЕРЕХОД ИЗ "Главная" В РАЗДЕЛ "Мой аккаунт" И НАЗАД НА ГЛАВНУЮ СТРАНИЦУ"
     @Test
-    public void TestHomePage_3() throws InterruptedException {
+    public void GoingToMyAccountTest()
+    {
 //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
-//        Использовал ожидание 1.5 секунды чтобы появились все элементы
-        Thread.sleep(1500);
 //        Элемент для перехода в раздел "Главная"
         var HomeElementLocator = By.cssSelector("li#menu-item-26");
 //        Элемент для перехода в раздел "Мой аккаунт"
@@ -115,16 +106,10 @@ public class TestHomePage {
         var TitleAccauntElementLocator = By.xpath("//h2[@class='entry-title']");
 //        Элемент блока главной страницы, подтверждающий нахождение пользователя на главной странице
         var PromoWrap1ElementLocator = By.xpath("//*[@class = 'promo-wrap1']");
-//        Текст названия раздела МОЙ АККАУНТ
-        var TextMyAccount = driver.findElement(AccauntElementLocator).getText();
 //        Нажатие в главном меню на раздел "Мой аккаунт"
         driver.findElement(AccauntElementLocator).click();
 //        Проверка на нахождение элемента, который подтверждает что мы в разделе "Мой аккаунт"
         Assert.assertTrue("Элемент не найден", driver.findElement(TitleAccauntElementLocator).isDisplayed());
-//        Текст заголовка раздела МОЙ АККАУНТ
-        var TitleTextMyaccount = driver.findElement(TitleAccauntElementLocator).getText();
-//        Проверка на совпадение текста названия раздела МОЙ АККАУНТ с заголовком раздела МОЙ АККАУНТ
-        Assert.assertEquals("Неверный текст", TextMyAccount, TitleTextMyaccount);
 //        Нажатие в главном меню на раздел "Главная"
         driver.findElement(HomeElementLocator).click();
 //        Проверка на нахождение элемента, который подтверждает что мы в разделе "Главная"
@@ -137,7 +122,7 @@ public class TestHomePage {
 //----------------------------------------------------------------------------------------------------------------------
 //  ТЕСТ НА ПЕРЕХОД ИЗ РАЗДЕЛА "Главная" В РАЗДЕЛ "Корзина" И ВОЗВРАЩЕНИЕ НА ГЛАВНУЮ СТРАНИЦУ САЙТА
     @Test
-    public void TestHomePage_4()
+    public void GoingToMyBasketTest()
     {
         //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
@@ -164,31 +149,25 @@ public class TestHomePage {
 
 
 //----------------------------------------------------------------------------------------------------------------------
-//  ПЕРЕХОД ИЗ РАЗДЕЛА "Главная" В РАЗДЕЛ "Оформление заказа"
-//  Переделать
+//  ПЕРЕХОД ИЗ РАЗДЕЛА "Главная" В РАЗДЕЛ "Оформление заказа", если пользователь не авторизован
+
+//  Здесь при клике на ОФОРМЛЕНИЕ ЗАКАЗА осуществляется переход в раздел КОРЗИНА, поэтому наблюдаем падение теста
+//  Переход в раздел ОФОРМЛЕНИЕ ЗАКАЗА осуществляется только после добавления товара в корзину.
     @Test
-    public void TestHomePage_5()
+    public void GoingToOrderRegistrationTest()
     {
 //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
-//        Элемент для перехода в раздел "Главная"
-        var HomeElementLocator = By.xpath("(//a[@aria-current='page'])[1]");
 //        Элемент для перехода в раздел "Оформление заказа"
         var OformlenieElementLocator = By.cssSelector("#menu-item-31 > a");
-//        Элемент заголовка раздела "Оформление заказа"
-//        var TitleOformlenieElementLocator = By.xpath("//h2[@class = 'entry-title'][text() = 'Оформление заказа']");;
 //        Элемент заголовка раздела "Корзина"
         var TitleKorzinaElementLocator = By.xpath("//h2[@class = 'entry-title']");
-//        Элемент подтверждающий нахождения в разделе "Главная"
-        var HomeElement = By.className("apwidget_title");
+//        Элемент заголовка раздела ОФОРМЛЕНИЕ ЗАКАЗА
+        var TitleOformlenieZakazaLocator = By.xpath("//h2[@class='post-title']");
 //        Нажатие в главном меню на раздел "Оформление заказа"
         driver.findElement(OformlenieElementLocator).click();
 //        Проверка на нахождение элемента, который подтверждает что мы в разделе "Оформление заказа"
-//        Assert.assertTrue("Элемент не найден", driver.findElement(TitleKorzinaElementLocator).isDisplayed());
-//        Нажатие в главном меню на раздел "Главная"
-//        driver.findElement(HomeElementLocator).click();
-//        Проверка на нахождение элемента, который подтверждает что мы в разделе "Главная"
-//        Assert.assertTrue("Элемент не найден", driver.findElement(HomeElement).isDisplayed());
+        Assert.assertTrue("Элемент не найден", driver.findElement(TitleOformlenieZakazaLocator).isDisplayed());
     }
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -197,7 +176,7 @@ public class TestHomePage {
 //   ПРОВЕРКА НА НАЛИЧИЕ ПОСЛЕ АВТОРИИЗАЦИИ ПОЛЬЗОВАТЕЛЯ ПРИВЕТСТВЕННОГО СООБЕЩЕНИЯ
 //   В ВЕРХНЕМ ПРАВОМ УГЛУ"Привет Sacha !"
     @Test
-    public void TestHomePage_6()
+    public void GoingToWelcomeMessageTest()
     {
 //       Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
@@ -230,24 +209,32 @@ public class TestHomePage {
 //----------------------------------------------------------------------------------------------------------------------
 //    ПЕРЕХОД В БЛОК КНИГИ ИЗ РАЗДЕЛА "Главная"
     @Test
-    public void TestHomePage_7()
+    public void GoingToBookBlockTest()
     {
 //        Открытие  страницы сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
 //         Элемент для перехода в раздел "Главная"
         var HomeElementLocator = By.xpath("(//div[@class='store-menu']//a)[1]");
-//        Элемент "Просмотреть" раздела "Книги"
-        var SpanLinkBtnLocator = By.xpath("(//span[@class = 'btn promo-link-btn'])[1]");
+//          Элемент блока КНИГИ
+        var KnigiLocator = By.xpath("(//*[@class='caption wow fadeIn'])[1]");
 //        Элемент заголовка раздела "Книги"
         var TitleKnigiLocator = By.xpath("(//h1)[2]");
 //        Элемент подтверждающий нахождения в разделе "Главная"
         var HomeElement = By.className("apwidget_title");
-//        Клик по кнопке "Просмотреть" раздела "Книги"
-        driver.findElement(SpanLinkBtnLocator).click();
-//        Проверка на наличие заголовка "книги"
+//        Ожидание появления, в блоке КНИГИ, элементов: книги, посмотреть
+        wait.until(ExpectedConditions.elementToBeClickable(KnigiLocator));
+//        Элемент заголовка блока КНИГИ
+        var NameKnigiLocator = By.xpath("(//*[@class='caption wow fadeIn'])[1]//h4");
+//        Текст названия блока КНИГИ
+        var TextNameKnigiLocator = driver.findElement(NameKnigiLocator).getText();
+//        Клик по блоку КНИГИ
+        driver.findElement(KnigiLocator).click();
+//        Проверка на наличие заголовка раздела КНИГИ
         Assert.assertTrue("Элемент не найден", driver.findElement(TitleKnigiLocator).isDisplayed());
+//        Текст заголовка раздела КНИГИ
+        var TextTitleKnigiLocator  = driver.findElement(TitleKnigiLocator).getText();
 //        Проверка на совпадение текста названия блока "Книги" с названем заголовка раздела
-        Assert.assertEquals("Неверный текст !", "КНИГИ", driver.findElement(TitleKnigiLocator).getText());
+        Assert.assertEquals("Неверный текст !", TextNameKnigiLocator, TextTitleKnigiLocator);
 //        Переход на главную страницу
         driver.findElement(HomeElementLocator).click();
 //        Проверка подтверждающая нахождение на главной странице
@@ -258,52 +245,101 @@ public class TestHomePage {
 
 
 //----------------------------------------------------------------------------------------------------------------------
-//    ТЕСТ НА ПЕРЕХОД В РАЗДЕЛ ПЛАНШЕТЫ БЕЗ ВХОДА В ЛИЧНЫЙ КАБИНЕТ
+//    ТЕСТ НА ПЕРЕХОД В БЛОК ПЛАНШЕТЫ ИЗ РАЗДЕЛА "Главная"
     @Test
-    public void TestHomePage_8() throws InterruptedException {
+    public void GoingToBlockTabletsTest()
+    {
 //        Открытие сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
-
-//        Использовал ожидание 1.5 секунды чтобы появились все элементы
-//        Так как способ wait.until(ExpectedConditions.presenceOfElementLocated(TabletsElementLocator));
-//        почему-то не сработал, я и время менял, но тест завершался до того как нужный элемент
-//        (название блока ПЛАНШЕТЫ) появлялся
-        Thread.sleep(1500);
-
-//        Элемент блока ПЛАНШЕТЫ
-        var TabletsElementLocator = By.cssSelector("aside#accesspress_storemo-3");
 //        Элемент заголовка раздела ПЛАНШЕТЫ
         var TitleTabletsElementLocator = By.cssSelector("h1.entry-title.ak-container");
 //        Элемент ГЛАВНАЯ в разделе ПЛАНШЕТЫ
         var MainElementlocator = By.xpath("//a[@href = 'http://intershop5.skillbox.ru']");
 //        Элемент блока главной страницы, подтверждающий нахождение пользователя на главной странице
         var PromoWrap1ElementLocator = By.xpath("//*[@class = 'promo-wrap1']");
-//          Элемент названия блока ПЛАНШЕТЫ
-        var Planshet = By.xpath("(//h4)[2]");
+//          Элемент блока ПЛАНШЕТЫ
+        var PlanshetLocator = By.xpath("(//*[@class='caption wow fadeIn'])[2]");
+//        Элемент заголовка блока ПЛАНШЕТЫ
+        var TitlePlanshetLocator = By.xpath("(//*[@class='caption wow fadeIn'])[2]//h4");
+//          Ожидание появления, в блоке ПЛАНШЕТЫ, элементов: планшеты, посмотреть
+        wait.until(ExpectedConditions.elementToBeClickable(PlanshetLocator));
 //          Текст названия блока ПЛАНШЕТЫ на главной странице
-        var TextBlockTablets = driver.findElement(Planshet).getText();
+        var TextTitlePlanshet = driver.findElement(TitlePlanshetLocator).getText();
 //          Клик по элементу блока ПЛАНШЕТЫ
-        driver.findElement(TabletsElementLocator).click();
+        driver.findElement(PlanshetLocator).click();
 //        Проверка на наличие заголовка ПЛАНШЕТЫ в разделе ПЛАНШЕТЫ
         Assert.assertTrue("Элемент не найден", driver.findElement(TitleTabletsElementLocator).isDisplayed());
 //        Текст заголовка раздела ПЛАНШЕТЫ
         var TextTitleTablets = driver.findElement(TitleTabletsElementLocator).getText();
 //        Проверка текста на совпадение названия блока ПЛАНШЕТЫ с текстом заголовка раздела ПЛАНШЕТЫ
-        Assert.assertEquals("Неверный текст", TextBlockTablets, TextTitleTablets);
+        Assert.assertEquals("Неверный текст", TextTitlePlanshet, TextTitleTablets);
 //        Клик по элементу ГЛАВНАЯ в разделе ПЛАНШЕТЫ
         driver.findElement(MainElementlocator).click();
 //        Проверка на наличие элемента  блока главной страницы, подтверждающий нахождение пользователя на главной странице
         Assert.assertTrue("Элемент не найден", driver.findElement(PromoWrap1ElementLocator).isDisplayed());
     }
+//----------------------------------------------------------------------------------------------------------------------
+//    Тест перехода в блок ФОТОАППАРАТЫ
+//    Здесь не соответствие заголовков при переходе в блок ФОТОАППАРТЫ
+//    ФОТОАППАРАТЫ - ФОТО/ВИДЕО
 
     @Test
-    public void TEstProverka() throws InterruptedException {
-//        driver.get("http://intershop5.skillbox.ru/");
+    public void GoingToTheCameraUnitTest()
+    {
+//        Открытие сайта
         driver.navigate().to("http://intershop5.skillbox.ru/");
-        Thread.sleep(5000);
-        driver.manage().window().setSize(new Dimension(1296, 696));
-        driver.findElement(By.cssSelector("#menu-item-31 > a")).click();
-        Thread.sleep(3000);
-//        driver.findElement(By.cssSelector("#menu-item-26 > a")).click();
+//        Элемент блока ФОТОАППАРАТЫ
+        var CameraUnitElementLocator = By.xpath("(//*[@class='caption wow fadeIn'])[3]");
+//        Элемент заголовка раздела ФОТОАППАРТЫ
+        var TitleCameraUnitElementLocator = By.xpath("//h1[@class='entry-title ak-container']");
+//        Элемент ГЛАВНАЯ в разделе ФОТОАППАРАТЫ
+        var MainElementlocator = By.xpath("//div[@class='woocommerce-breadcrumb accesspress-breadcrumb']//a[1]");
+//        Элемент блока главной страницы, подтверждающий нахождение пользователя на главной странице
+        var PromoWrap1ElementLocator = By.xpath("//*[@class = 'promo-wrap1']");
+//        Элемент заголовка блока ФОТОАППАРТЫ
+        var TitleBlockCameraUnitLocator = By.xpath("(//*[@class='caption wow fadeIn'])[3]//h4");
+//          Ожидание появления, в блоке ФОТОАППАРАТЫ, элементов: фотоаппараты, посмотреть
+        wait.until(ExpectedConditions.elementToBeClickable(CameraUnitElementLocator));
+//          Текст названия блока ФОТОАППАРАТЫ на главной странице
+        var TextTitleBlockCameraUnit = driver.findElement(TitleBlockCameraUnitLocator).getText();
+//          Клик по элементу блока ФОТОАППАРАТЫ
+        driver.findElement(CameraUnitElementLocator).click();
+//        Проверка на наличие заголовка ФОТО/ВИДЕО в разделе ФОТОАППАРАТЫ
+        Assert.assertTrue("Элемент не найден", driver.findElement(TitleCameraUnitElementLocator).isDisplayed());
     }
+//----------------------------------------------------------------------------------------------------------------------
+// Тест на переход для просмотра товара IPAD AIR 2020 через баннер УЖЕ В ПРОДАЖЕ
+    @Test
+    public void GoingToAlreadyOnSaleTest()
+    {
+//        Открытие сайта
+        driver.navigate().to("http://intershop5.skillbox.ru/");
+//        Элемент банера УЖЕ В ПРОДАЖЕ
+        var CameraUnitElementLocator = By.xpath("(//div[@class='promo-image'])[4]");
+//        Элементы на баннере УЖЕ В ПРОДАЖЕ
+        var BanerElementLocator = By.xpath("(//*[@class='caption wow fadeIn'])[4]");
+//        Название товара УЖЕ В ПРОДАЖЕ
+        var AlreadyOnSaleElementLocator = By.xpath("(//*[@class='caption wow fadeIn'])[4]//div[@class='promo-desc-title']");
+//        Название товара, которое указано в разделе (ВСЕ ТОВАРЫ) после клика по баннеру УЖЕ В ПРОДАЖЕ
+        var TitleProductElementLocator = By.xpath("//h1[@class='product_title entry-title']");
+//        Элемент ГЛАВНАЯ в раздела ВСЕ ТОВАРЫ
+        var MainElementlocator = By.xpath("//div[@class='woocommerce-breadcrumb accesspress-breadcrumb']//a[1]");
+//        Элемент блока главной страницы, подтверждающий нахождение пользователя на главной странице
+        var PromoWrap1ElementLocator = By.xpath("//*[@class = 'promo-wrap1']");
+        //        Ожидание всех элементов на банере УЖЕ В ПРОДАЖЕ
+        wait.until(ExpectedConditions.elementToBeClickable(BanerElementLocator));
+//        Клик по банеру УЖЕ В ПРОДАЖЕ
+        driver.findElement(CameraUnitElementLocator).click();
+////        Проверка на наличие заголовка ПЛАНШЕТЫ в разделе ПЛАНШЕТЫ
+//        Assert.assertTrue("Элемент не найден", driver.findElement(TitleTabletsElementLocator).isDisplayed());
+////        Текст заголовка раздела ПЛАНШЕТЫ
+//        var TextTitleTablets = driver.findElement(TitleTabletsElementLocator).getText();
+////        Проверка текста на совпадение названия блока ПЛАНШЕТЫ с текстом заголовка раздела ПЛАНШЕТЫ
+//        Assert.assertEquals("Неверный текст", TextTitlePlanshet, TextTitleTablets);
+////        Клик по элементу ГЛАВНАЯ в разделе ПЛАНШЕТЫ
+//        driver.findElement(MainElementlocator).click();
+////        Проверка на наличие элемента  блока главной страницы, подтверждающий нахождение пользователя на главной странице
+//        Assert.assertTrue("Элемент не найден", driver.findElement(PromoWrap1ElementLocator).isDisplayed());
+    }
+
 }
